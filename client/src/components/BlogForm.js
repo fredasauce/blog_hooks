5f8358dc3f434435d80f109d0db984f3d6c2853e
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-19 12:40:32 -0600

    custom hooks in blogform, navbar, and fetchuser

--- client/src/components/BlogForm.js
+++ client/src/components/BlogForm.js
@@ -1,14 +1,15 @@
 import React, { useState, } from "react"
 import axios from "axios"
 import { Form, } from "semantic-ui-react"
+import { useFormInput, } from "../hooks/useFormInput"
 
 const BlogForm = (props) => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const title = useFormInput("");
+  const body = useFormInput("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("/api/blogs", { title, body, })
+    axios.post("/api/blogs", { title: title.value, body: body.value, })
       .then( res => {
         props.add(res.data);
         props.toggleForm();
@@ -23,16 +24,14 @@ const BlogForm = (props) => {
           placeholder="Title"
           name="title"
           required
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
+          { ...title }
         />
         <Form.Input 
           label="Body"
           placeholder="Body"
           name="body"
           required
-          onChange={(e) => setBody(e.target.value)}
-          value={body}
+          { ...body }
         />
       </Form.Group>
       <Form.Button>Submit</Form.Button>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { AuthConsumer, } from "../providers/AuthProvider";
+import React, { useContext, } from 'react'
+import { AuthContext, } from "../providers/AuthProvider";
 import { Menu, } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
 
-class Navbar extends React.Component {
-  
-  rightNavItems = () => {
-    const { auth: { user, handleLogout, }, location, } = this.props;
-    
+const Navbar = ({history, location,}) => {
+  const { user, handleLogout } = useContext(AuthContext);
+
+  const rightNavItems = () => {    
     if (user) {
       return (
         <Menu.Menu position='right'>
+          <Menu.Item>
+            { user.email }
+          </Menu.Item>
           <Menu.Item
             name='logout'
-            onClick={ () => handleLogout(this.props.history) }
+            onClick={ () => handleLogout(history) }
           />
         </Menu.Menu>
       )
@@ -38,42 +40,102 @@ class Navbar extends React.Component {
       )
     }
   }
+
+  return (
+    <div>
+      <Menu pointing secondary>
+        <Link to='/'>
+          <Menu.Item
+            name='home'
+            id='home'
+            active={location.pathname === '/'}
+          />
+        </Link>
+        <Link to='/blogs'>
+          <Menu.Item
+            name='blogs'
+            id='blogs'
+            active={location.pathname === '/blogs'}
+          />
+        </Link>
+          { rightNavItems() }
+      </Menu>
+    </div>
+  );
+};
+
+// class Navbar extends React.Component {
   
-  render() {
-    return (
-      <div>
-        <Menu pointing secondary>
-          <Link to='/'>
-            <Menu.Item
-              name='home'
-              id='home'
-              active={this.props.location.pathname === '/'}
-            />
-          </Link>
-          <Link to='/blogs'>
-            <Menu.Item
-              name='blogs'
-              id='blogs'
-              active={this.props.location.pathname === '/blogs'}
-            />
-          </Link>
-            { this.rightNavItems() }
-        </Menu>
-      </div>
-    )
-  }
-}
+//   rightNavItems = () => {
+//     const { auth: { user, handleLogout, }, location, } = this.props;
+    
+//     if (user) {
+//       return (
+//         <Menu.Menu position='right'>
+//           <Menu.Item
+//             name='logout'
+//             onClick={ () => handleLogout(this.props.history) }
+//           />
+//         </Menu.Menu>
+//       )
+//     } else {
+//       return (
+//         <Menu.Menu position='right'>
+//           <Link to='/login'>
+//             <Menu.Item
+//               id='login'
+//               name='login'
+//               active={location.pathname === '/login'}
+//             />
+//           </Link>
+//           <Link to='/register'>
+//             <Menu.Item
+//               id='register'
+//               name='register'
+//               active={location.pathname === '/register'}
+//             />
+//           </Link>
+//         </Menu.Menu>
+//       )
+//     }
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+//         <Menu pointing secondary>
+//           <Link to='/'>
+//             <Menu.Item
+//               name='home'
+//               id='home'
+//               active={this.props.location.pathname === '/'}
+//             />
+//           </Link>
+//           <Link to='/blogs'>
+//             <Menu.Item
+//               name='blogs'
+//               id='blogs'
+//               active={this.props.location.pathname === '/blogs'}
+//             />
+//           </Link>
+//             { this.rightNavItems() }
+//         </Menu>
+//       </div>
+//     )
+//   }
+// }
 
-export class ConnectedNavbar extends React.Component {
-  render() {
-    return (
-      <AuthConsumer> 
-        { auth => 
-          <Navbar { ...this.props } auth={auth} />
-        }
-      </AuthConsumer>
-    )
-  }
-}
+// export class ConnectedNavbar extends React.Component {
+//   render() {
+//     return (
+//       <AuthConsumer> 
+//         { auth => 
+//           <Navbar { ...this.props } auth={auth} />
+//         }
+//       </AuthConsumer>
+//     )
+//   }
+// }
 
-export default withRouter(ConnectedNavbar);
+// export default withRouter(ConnectedNavbar);
+export default withRouter(Navbar);
